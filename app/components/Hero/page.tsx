2232dbfd42c0f8c6137cbffd930ab3b4ca65f613
Jacob Bello <jacobbello2002@gmail.com>
2026-01-08 18:55:03 -0800

    hero content with no button

---
+++ app/components/Hero/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import styles from './page.module.css'
+
+const page = () => {
+  return (
+    <div className={styles.hero}>
+        <div className={styles.content}>
+        <h1>Red Lobster</h1>
+        <h2>Experience ocean-fresh seafood in an elegant waterfront setting</h2>
+      </div>
+    </div>
+  )
+}
+
+export default page
